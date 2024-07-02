8aa6ae5302b6e8fe76f58b1b7375f9b49eb3cd7f
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2024-07-02 23:41:17 +0530

    navbar basic item and animation

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,36 @@
 import "./style.scss";
 
+import useDimension from "../../hooks/useDimension";
+import useDisclosure from "../../hooks/useDisclosure";
+
+import { motion } from "framer-motion";
+import NavbarItem from "../NavbarItem";
+
 const Navbar = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { width } = useDimension();
+
   return (
     <div className="navbar-container">
-      <p className="navbar-logo">
+      <motion.div
+        initial={{ width: 0, opacity: 0 }}
+        animate={{
+          width: isOpen ? "20vw" : 0,
+          opacity: isOpen ? 1 : 0,
+          userSelect: "none",
+        }}
+        className="navbar-overlay"
+      >
+        <button onClick={onClose}>close</button>
+        <div className="navbar-items-container">
+          <h4>List of Universities</h4>
+          <NavbarItem title="KTU" />
+          <NavbarItem title="Calicut" />
+          <NavbarItem title="MG" />
+        </div>
+      </motion.div>
+      <p className="navbar-right">
+        <div className="navbar-toggle" onClick={onOpen} />
         Blinko <span>( beta )</span>
       </p>
       <p className="navbar-version">v0.3</p>
