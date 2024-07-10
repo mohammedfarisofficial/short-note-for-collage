18726515382100085aaad6de65a3d3040f452cb4
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2024-07-10 21:38:42 +0530

    list of stream and semester with route

--- server/routes/semester.js
+++ server/routes/semester.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createSemester } from "../controllers/semester.js";
+import { createSemester, getSemesters } from "../controllers/semester.js";
 
 const router = Router();
 
 router.post("/", createSemester);
+router.get("/:streamId", getSemesters);
 
 export default router;
